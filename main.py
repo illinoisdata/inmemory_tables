064a895326b4d4e454c0649e43126b984f3f0529
BillyZhaohengLi <zl20@illinois.edu>
2022-02-16 16:39:32 -0600

    added command line args

--- main.py
+++ main.py
@@ -3,34 +3,28 @@ from ExecutionGraph import *
 from Optimizer import *
 from tpcds_queries import *
 import polars as pl
+import argparse
 
-import threading, queue
-import time
-import matplotlib.pyplot as plt
-import sns
-import copy
-import utils
-import os.path
-import os
-import cProfile, pstats
-import psutil
-import scipy.stats
-
-if __name__ == '__main__':   
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-Q", "--query", help="TPC-DS query number")
+    parser.add_argument("-M", "--memory", help="Memory container size")
+    args = parser.parse_args()
+    
     # Get node representation of a TPC-DS query 
-    execution_nodes = get_tpcds_query_nodes(query_num = 5)
+    execution_nodes = get_tpcds_query_nodes(query_num = int(args.query))
 
     # Create graph & add nodes
     execution_graph = ExecutionGraph()
     execution_graph.add_nodes(execution_nodes)
-    execution_graph.build_graph(draw_graph = True)
+    execution_graph.build_graph(draw_graph = False)
 
     # Dry run; store no nodes in memory
     execution_graph.execute(debug = True)
     
     # Create optimizer to jointly optimize nodes to store in memory & execution
     # order
-    optimizer = Optimizer(execution_graph, memory_limit = 200000000)
+    optimizer = Optimizer(execution_graph, memory_limit = int(args.memory))
     optimizer.optimize(debug = True)
 
     # Evaluate efficiency after optimization
