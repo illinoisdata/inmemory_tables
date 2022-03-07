b0db763c901edf16a8559ea3050b66d96558b782
BillyZhaohengLi <zl20@illinois.edu>
2022-03-07 16:45:33 -0600

    updated parquet converter

--- to_parquet.py
+++ to_parquet.py
@@ -7,10 +7,30 @@ from tpcds_queries_new import *
 import polars as pl
 import argparse
 
+def read_columns():
+    column_dict = defaultdict(list)
+    column_file = open("tpcds/column_list.txt")
+    for line in column_file.readlines():
+        if line[0].isalpha():
+            col_table = line.split()
+            table = col_table[1].lower()
+            col = col_table[0].lower()
+            column_dict[col_table[1].lower()].append(col_table[0].lower())
+
+    return column_dict
+
+def read_table(table_name, column_dict):
+    # test database
+    f = open("tpcds/" + table_name + ".dat", "r", encoding = "utf-8",
+             errors = 'replace')
+    df_str = f.read()
+    column_count = df_str.split('\n')[0].count('|')
+    return pl.read_csv(io.StringIO(df_str), has_header = False, sep = '|',
+                       new_columns = column_dict[table_name][:column_count])
+
 if __name__ == '__main__':
-    tablereader = TableReader()
-    tablereader.read_columns()
+    column_dict = read_columns
     
-    for column in tablereader.column_dict.keys():
-        table = tablereader.read_table(column)
+    for column in column_dict.keys():
+        table = read_table(column)
         parquet_result(table, column, location = 'tpcds/')
