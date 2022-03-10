515f603d84f50b922baedb07bbca805759b044af
BillyZhaohengLi <zl20@illinois.edu>
2022-03-09 18:38:18 -0600

    improved tablereader

--- TableReader.py
+++ TableReader.py
@@ -13,6 +13,7 @@ class TableReader(object):
     """
     def __init__(self):
         self.table_read_times = defaultdict(int)
+        self.table_read_num_times = defaultdict(int)
 
     def read_table(self, table_name):
         start = time.time()
@@ -21,15 +22,19 @@ class TableReader(object):
         
         table_read_time = time.time() - start
         self.table_read_times[table_name] += table_read_time
+        self.table_read_num_times[table_name] += 1
 
         return table
 
     def report(self):
         print("table read times:")
         for k, v in self.table_read_times.items():
-            print(k, v)
+            print("Table:" + str(k) + " times read: " +
+                  str(self.table_read_num_times[k]) + " total time: " +
+                  str(v))
         print("total table read time:",
               sum(list(self.table_read_times.values())))
 
     def clear(self):
         self.table_read_times = defaultdict(int)
+        self.table_read_num_times = defaultdict(int)
