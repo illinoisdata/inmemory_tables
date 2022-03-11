f4e0ba6402d36460eff6a775c585ff9d24177de6
BillyZhaohengLi <zl20@illinois.edu>
2022-03-11 00:42:20 -0600

    added shell script for experiments

---
+++ experiment.sh
@@ -0,0 +1,16 @@
+for i in 1 2 3 4 5
+	for j in 2 5
+		do
+			python3 setting_generator.py --job $i --memory $j
+			python3 setting_consumer.py --job $i --memory $j --store mkp --top both
+			python3 setting_consumer.py --job $i --memory $j --store all --top none
+			python3 setting_consumer.py --job $i --memory $j --store none --top none
+			python3 setting_consumer.py --job $i --memory $j --store greedy --top none
+			python3 setting_consumer.py --job $i --memory $j --store random --top none
+			python3 setting_consumer.py --job $i --memory $j --store mkp --top none
+			python3 setting_consumer.py --job $i --memory $j --store greedy --top both
+			python3 setting_consumer.py --job $i --memory $j --store random --top both
+			python3 setting_consumer.py --job $i --memory $j --store mkp --top dfs
+			python3 setting_consumer.py --job $i --memory $j --store mkp --top sa
+			python3 setting_consumer.py --job $i --memory $j --store mkp --top recursive_min_cut
+		done
