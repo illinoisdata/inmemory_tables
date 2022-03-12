c11cc3fbf92ea970efe3fdb0353c6a8ac0517864
BillyZhaohengLi <zl20@illinois.edu>
2022-03-12 10:37:39 -0600

    fixed typo

--- to_parquet.py
+++ to_parquet.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
         table = read_table(column, column_dict)
 
         p_size = 5
-        num_partitions = int(math.ceil(column_dict[column] / p_size))
+        num_partitions = int(math.ceil(len(column_dict[column]) / p_size))
         for i in range(num_partitions):
             if (i * p_size > len(column_dict[column])):
                 parquet_result(table[column_dict[column][i*p_size:]],
